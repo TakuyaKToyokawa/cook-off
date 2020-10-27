743ff1f1240c7aa6717a7706377ebc2b25db5fbb
TakuyaToyokawa <takuya.k.toyokawa@gmail.com>
2020-10-26 18:15:10 -0700

    first commit

---
+++ stories/FoodList.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import FoodList from '../comps/FoodList';
+
+export default {
+  title: 'Events/FoodList',
+  component: FoodList,
+};
+
+export const BaseFoodList = () => <FoodList/>;
---
+++ stories/MenuBar.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MenuBar from '../comps/MenuBar';
+
+export default {
+  title: 'General/MenuBar',
+  component: MenuBar,
+};
+
+export const BaseMenuBar = () => <MenuBar/>;
