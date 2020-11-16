f83e3d936de5a0073e0c9b1c80af8c71dec34a4d
Your Name <you@example.com>
2020-11-16 13:41:33 -0800

    some more setting pages done

--- pages/appearance.js
+++ pages/appearance.js
@@ -2,6 +2,9 @@ import NavigationHeader from "../comps/NavigationHeader";
 import MenuBar from "../comps/MenuBar";
 import MenuList from "../comps/MenuList";
 import FontSizeSlider from "../comps/FontSizeSlider";
+import OnOffInput from "../comps/OnOffInput";
+
+
 
 
 function Appearance() {
@@ -9,16 +12,19 @@ function Appearance() {
     <main className="main">
     <div className="content">
     <div className="headerSetting"><NavigationHeader text="Setting"/></div>
-     <div><h2>Appearance</h2></div>
+     <div className="title">
+     <div className="titleImg"><img src="./appearance.svg"/></div>
+       <h2>Appearance</h2></div>
      <div className="smallTitle"><h5>Font-size</h5></div>
 <div className="settingList"><FontSizeSlider/></div>
 <div className="settingList">
   <MenuList icon="./appearance.svg" text="Darkmode/lightmode"/>
- 
+  <OnOffInput/>
 
 </div>
+<MenuBar/>
    </div>
-      <MenuBar/>
+
   </main>
 
   );
